679a96abc4386889f4da78f78264328e6ec26376
anujdarji2001 <anujdarji2001@gmail.com>
2022-06-15 12:02:50 +0530

    first commit

---
+++ accounts/views.py
@@ -0,0 +1,103 @@
+from django.shortcuts import render,redirect
+from .models import *
+from django.contrib.auth import authenticate
+from django.views.decorators.cache import cache_control
+
+# Create your views here.
+
+def home(request):
+    if request.session.get('vendor') == None:
+        return redirect('login')
+
+    cuser = request.session.get('vendor_email')
+    usr = CustomUser.objects.get(email=cuser)
+    return render(request, 'app/index.html',{'usr':usr})
+
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+def signup(request):
+
+    if request.session.get('vendor') != None:
+        return redirect('home')
+
+    department = ['patient', 'doctor']
+
+    if request.method == 'POST':
+        dname = request.POST['dname']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        propic = request.FILES['propic']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password1']
+        confirm_password = request.POST['password2']
+        address = request.POST['address']
+        city = request.POST['city']
+        state = request.POST['state']
+        pincode = request.POST['pincode']
+
+        if CustomUser.objects.filter(username=username):
+            msg = 'Username already exist please try some other Username'
+            return render(request,'app/signup.html',{'department':department,'msg':msg})
+
+        if CustomUser.objects.filter(email=email):
+            msg = 'Email alredy registred'
+            return render(request,'app/signup.html',{'department':department,'msg':msg})
+        
+        if (password == confirm_password):
+            usr = CustomUser.objects.create_user(user_type=dname,first_name=fname,last_name=lname,propic= propic,
+                                username=username,email=email,password=password,address=address,
+                                city=city,state=state,pincode=pincode)
+            usr.is_active = True
+            usr.is_staff = True
+            usr.save()
+            return redirect('login')
+            
+        else:
+            msg = "Password are not matched!!!"
+            return render(request,'app/signup.html',{'department':department,'msg':msg})
+
+    return render(request,'app/signup.html',{'department':department})
+
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+def login(request):
+
+    department = ['patient', 'doctor']
+
+    if request.session.get('vendor') != None:
+        return redirect('home')
+
+    if request.method == 'POST':
+        email = request.POST['email']
+        pwd = request.POST['password']
+        dname = request.POST['dname']
+
+        print(email,pwd)
+        usr = authenticate(email=email,password=pwd)
+        print(usr)
+        if usr:
+            usr = CustomUser.objects.get(email=email)
+            if usr.user_type == dname :
+                if (usr.is_staff == True):
+                    vendor = {'vendor_name':usr.username,'vendor_email':usr.email}
+                    request.session['vendor'] = vendor
+                    request.session['vendor_email'] = usr.email
+                    return redirect('home')
+                else:                
+                    return render(request,'app/login.html',{'department':department})
+            else:
+                msg = "Incorrect Details!!!"
+                return render(request,'app/login.html',{'msg':msg,'department':department})
+        else:
+            msg = "Incorrect Email or Password!!!"
+            return render(request,'app/login.html',{'msg':msg,'department':department})
+    
+    return render(request,'app/login.html',{'department':department})
+
+
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+def logout(request):
+    if (request.session.get('vendor') != None):
+        request.session.delete()
+        return redirect('login')
+    else:
+        return redirect('login')
